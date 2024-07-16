f7c4baddabddb463e68c560be078d743347bb1b3
Leet <76251617+skyline523@users.noreply.github.com>
2024-07-16 16:56:45 +0800

    chore: 更新footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="mt-10 mb-6 prose m-auto flex items-center slide-enter animate-delay-1200!">
     <span class="text-sm op50"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2023-PRESENT © Leet</span>
     <img
-      class="hidden md:inline ml-3 h-4 object-contain my-0 w-auto op50"
+      class="hidden md:inline ml-3 h-4 object-contain !my-0 !w-auto op50"
       src="https://visitor-badge.laobi.icu/badge?page_id=skyline523.leet"
       onerror="this.style.display='none'"
     >
