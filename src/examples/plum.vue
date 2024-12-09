48371949093318e517ea4012eccc08a6d6d6c92c
Leet <76251617+skyline523@users.noreply.github.com>
2024-12-09 22:12:30 +0800

    fix: css-effects plum animation

--- src/examples/plum.vue
+++ src/examples/plum.vue
@@ -1,106 +1,125 @@
-<script setup lang="ts">
+<script setup="props" lang="ts">
+import { useRafFn } from '@vueuse/core'
 import type { Fn } from '@vueuse/core'
 
-const el = ref<HTMLCanvasElement>()
-const ctx = computed(() => el.value!.getContext('2d')!)
+const r180 = Math.PI
+const r90 = Math.PI / 2
+const r15 = Math.PI / 12
+const color = '#88888850'
 
-const WIDTH = 300
-const HEIGHT = 300
+const el = ref<HTMLCanvasElement | null>(null)
 
-const key = 1
+const { random } = Math
 
-interface Point {
-  x: number
-  y: number
+const f = {
+  start: () => {},
 }
 
-interface Branch {
-  start: Point // 画线开始坐标
-  length: number // 画线长度(斜边)
-  theta: number // 画线的夹角大小，用于偏移垂直或水平
+const init = ref(5)
+const len = ref(5)
+const stopped = ref(false)
+
+function initCanvas(canvas: HTMLCanvasElement, width = 400, height = 400, _dpi?: number) {
+  const ctx = canvas.getContext('2d')!
+
+  const dpr = window.devicePixelRatio || 1
+  // @ts-expect-error vendor prefix
+  const bsr = ctx.webkitBackingStorePixelRatio || ctx.mozBackingStorePixelRatio || ctx.msBackingStorePixelRatio || ctx.oBackingStorePixelRatio || ctx.backingStorePixelRatio || 1
+
+  const dpi = _dpi || dpr / bsr
+
+  canvas.style.width = `${width}px`
+  canvas.style.height = `${height}px`
+  canvas.width = dpi * width
+  canvas.height = dpi * height
+  ctx.scale(dpi, dpi)
+
+  return { ctx, dpi }
 }
 
-function init() {
-  ctx.value.strokeStyle = '#88888850'
+function polar2cart(x = 0, y = 0, r = 0, theta = 0) {
+  const dx = r * Math.cos(theta)
+  const dy = r * Math.sin(theta)
+  return [x + dx, y + dy]
+}
 
-  const branch = {
-    start: { x: WIDTH / 2, y: HEIGHT },
-    length: 3,
-    theta: -Math.PI / 2,
+onMounted(async () => {
+  const canvas = el.value!
+  const { ctx } = initCanvas(canvas)
+  const { width, height } = canvas
+
+  let steps: Fn[] = []
+  let prevSteps: Fn[] = []
+
+  let iterations = 0
+
+  const step = (x: number, y: number, rad: number) => {
+    const length = random() * len.value
+
+    const [nx, ny] = polar2cart(x, y, length, rad)
+
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(nx, ny)
+    ctx.stroke()
+
+    const rad1 = rad + random() * r15
+    const rad2 = rad - random() * r15
+
+    if (nx < -100 || nx > 500 || ny < -100 || ny > 500)
+      return
+
+    if (iterations <= init.value || random() > 0.5)
+      steps.push(() => step(nx, ny, rad1))
+    if (iterations <= init.value || random() > 0.5)
+      steps.push(() => step(nx, ny, rad2))
   }
-  step(branch)
-}
 
-const pendingTasks: Fn[] = []
+  let controls: ReturnType<typeof useRafFn>
 
-function step(b: Branch, depth = 0) {
-  const end = getEndPoint(b)
-  drawBranch(b)
+  const frame = () => {
+    iterations += 1
+    prevSteps = steps
+    steps = []
 
-  if (depth < 6 || Math.random() < 0.5) {
-    pendingTasks.push(() => {
-      step({
-        start: end,
-        length: 1,
-        theta: b.theta - 0.3 * Math.random(),
-      }, depth + 1)
-    })
+    if (!prevSteps.length) {
+      controls.pause()
+      stopped.value = true
+    }
+    prevSteps.forEach(i => i())
   }
-  if (depth < 6 || Math.random() < 0.5) {
-    pendingTasks.push(() => {
-      step({
-        start: end,
-        length: 1,
-        theta: b.theta + 0.3 * Math.random(),
-      }, depth + 1)
-    })
+
+  controls = useRafFn(frame, { immediate: false })
+
+  f.start = () => {
+    controls.pause()
+    iterations = 0
+    ctx.clearRect(0, 0, width, height)
+    ctx.lineWidth = 1
+    ctx.strokeStyle = color
+    prevSteps = []
+    steps = random() < 0.5
+      ? [
+          () => step(0, random() * 400, 0),
+          () => step(400, random() * 400, r180),
+        ]
+      : [
+          () => step(random() * 400, 0, r90),
+          () => step(random() * 400, 400, -r90),
+        ]
+    controls.resume()
+    stopped.value = false
   }
-}
 
-function frame() {
-  const tasks = [...pendingTasks]
-  pendingTasks.length = 0
-  tasks.forEach(task => task())
-}
-
-let lastTime = performance.now()
-const interval = 1000 / 40 // 50fps
-function startFrame() {
-  requestAnimationFrame(() => {
-    if (performance.now() - lastTime > interval)
-      frame()
-    lastTime = performance.now()
-    startFrame()
-  })
-}
-
-startFrame()
-
-function lineTo(p1: Point, p2: Point) {
-  ctx.value.beginPath()
-  ctx.value.moveTo(p1.x, p1.y)
-  ctx.value.lineTo(p2.x, p2.y)
-  ctx.value.stroke()
-}
-
-function getEndPoint(b: Branch) {
-  return {
-    x: b.start.x + b.length * Math.cos(b.theta), // 邻边 = 夹角余弦 * 斜边
-    y: b.start.y + b.length * Math.sin(b.theta), // 对边 = 夹角正弦 * 斜边
-  }
-}
-
-function drawBranch(l: Branch) {
-  lineTo(l.start, getEndPoint(l))
-}
-
-onMounted(() => {
-  init()
+  f.start()
 })
 </script>
 
 <template>
   <div>
-    <canvas :key="key" ref="el" width="300" height="300" border />
+    <canvas ref="el" width="400" height="400" border />
+    <div hover:color-gray-600 duration-200 cursor-pointer mt-2 inline-block @click="f.start">
+      restart
+    </div>
   </div>
 </template>
